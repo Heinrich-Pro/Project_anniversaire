90f6578f88074b6cdc48a34f16e3b68d13a7d0d3
Heinrich-Pro <Heinrich-Pro@outlook.com>
2025-03-07 16:02:23 +0100

    Update forms.py

--- project/inscriptions/forms.py
+++ project/inscriptions/forms.py
@@ -1,9 +1,9 @@
 from django import forms
-from .models import Participant
+from .models import index
 
 class ParticipantForm(forms.ModelForm):
     class Meta:
-        model = Participant
+        model = index
         fields = ['nom', 'email', 'message']
         widgets = {
             'message': forms.Textarea(attrs={'rows': 4}),
