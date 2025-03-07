b77e29d4827103da975a7f3aaac02837e7f6f58c
Heinrich-Pro <Heinrich-Pro@outlook.com>
2025-03-07 15:17:58 +0100

    Update urls.py

--- project/inscriptions/urls.py
+++ project/inscriptions/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.inscription_view, name='inscription'),
+    path('', views.index_view, name='index'),
     path('confirmation/', views.confirmation_view, name='confirmation'),
     path('participants/', views.liste_participants_view, name='liste_participants'),
 ]
